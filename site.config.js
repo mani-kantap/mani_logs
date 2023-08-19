75076bce053c8fbba73596c8cb68df6efa6c89c4
Manikanta Panuganti <31481823+mani-kantap@users.noreply.github.com>
2023-08-19 08:15:16 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Mani",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Machine Learning Engineer | Data scientist | AI/ML SDE | MLOps",
+    bio: "Master's student in Data Science at University of Maryland, BC. Former MLE at Sylph.AI and Software Engineer at Thoughtworks, Quolum.",
+    email: "manikanta.dev@pm.me",
+    linkedin: "manikantap-dev",
+    github: "mani-kantap",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `mani_logs`,
+      href: "https://github.com/mani-kantap/mani_logs",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "mani_logs",
+    description: "welcome to mani_logs!",
   },
 
   // CONFIG configration (required)
